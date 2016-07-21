4d3caefdab6894dfcdbbe1b3daf6672281c8cb23
yanglin <yanglin@enlink-mob.com>
2016-07-21 10:28:22 +0800

    修改THINK.Ext为THINK.ext

--- doc/exmple/restful/Restful/Controller/Test.js
+++ doc/exmple/restful/Restful/Controller/Test.js
@@ -3,7 +3,7 @@
  * @return
  */
 
-export default class extends THINK.Ext('RestfulController') {
+export default class extends THINK.ext('RestfulController') {
     init(http){
         super.init(http);
     }

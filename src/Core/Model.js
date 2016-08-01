6e29c59576b61f1b4ddb085ce7327f8f2b84edac
yanglin <richenlin@gmail.com>
2016-08-01 13:01:01 +0800

    fix bug

--- src/Core/Model.js
+++ src/Core/Model.js
@@ -928,6 +928,7 @@ export default class extends base {
             if (THINK.isEmpty(parsedOptions.where)) {
                 // 如果存在主键数据 则自动作为更新条件
                 if (!THINK.isEmpty(this._data[pk])) {
+                    parsedOptions.where = {};
                     parsedOptions.where[pk] = this._data[pk];
                     delete this._data[pk];
                 } else {
